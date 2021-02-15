af80c2ef930415152a4fef1e151ebf8f7a8324ce
akvilina <akvilina.lapenaite@gmail.com>
2021-02-15 11:53:27 +0200

    array $routes + fn get() + fn resolve();

--- core/Router.php
+++ core/Router.php
@@ -12,8 +12,39 @@ namespace app\core;
  */
 class Router
 {
+    /**
+     * This will hold all routes.
+     *
+     * taip atrodys routes masyvas:
+     * routes = [
+     *
+     * ['get => [
+     *  ['/' => function return],
+     *  ['/about' => function return],
+     * ],
+     *
+     * ['post' => [
+     *  ['/' => function return],
+     *  ['/about' => function return],
+     * ]
+     * ]
+     *
+     * @var array
+     */
+    protected array $routes = [];
+
     public function __construct()
     {
         echo "This is router constructor";
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        var_dump($_SERVER);
     }
 }
