8e08323f24e53e6149fc18c86abc26ef661b8ec5
akvilina <akvilina.lapenaite@gmail.com>
2021-02-17 09:44:39 +0200

    getMethod function created

--- core/Request.php
+++ core/Request.php
@@ -4,27 +4,36 @@
 namespace app\core;
 
 
-
 class Request
 {
 
     /**
-* Get user page from url
-* [REQUEST_URI] => /almostLara/todos?id=5
-* extract /todos
-*
+     * Get user page from url
+     * [REQUEST_URI] => /almostLara/todos?id=5
+     * extract /todos
+     *
      * @return string
      */
-    public function getPath() : string //nurodoma, kas grizta
+    public function getPath(): string //nurodoma, kas grizta
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/'; //jei sita reiksme $_SERVER['REQUEST_URI'] nenusetinta - duodam '/'
         $questionMarkPosition = strpos($path, '?');
 
-        if  ($questionMarkPosition !== false) :
+        if ($questionMarkPosition !== false) :
             $path = substr($path, 0, $questionMarkPosition);
         endif;
 
         return $path;
 //        var_dump($questionMarkPosition);
     }
+
+    /**
+     * This will return http method get or post
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
 }
