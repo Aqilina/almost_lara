7ad6fa574ba13ca97e4632d28e7955fd295f12e2
akvilina <akvilina.lapenaite@gmail.com>
2021-02-18 11:15:48 +0200

    home and about pages controlled by side controllers

--- public/index.php
+++ public/index.php
@@ -16,11 +16,10 @@ use app\core\Application; //router inicijuojamas Application dalyje
 //kuriant nauja klase paduodama dirname
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-$app->router->get('/about', 'about');
-$app->router->get('/contact', [SiteController::class, 'contact']);
-
 //CREATE POST PATH. paduodamas klases pavadinimas - kaip kontroleris, handleContact metodas - kaip metodas
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/about', [SiteController::class, 'about']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']); //SiteController::class  - su namespace
 $app->run();
 
