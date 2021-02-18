51b8e8973ea62788d9d58c0cfe47395f34447feb
akvilina <akvilina.lapenaite@gmail.com>
2021-02-18 08:55:03 +0200

    Receive response as an argument and use it to set 404 error code

--- core/Router.php
+++ core/Router.php
@@ -33,10 +33,12 @@ class Router
      */
     protected array $routes = [];
     public Request $request;
+    public Response $response;
 
-    public function __construct($request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     /**
@@ -70,7 +72,7 @@ class Router
         //IF THERE ARE NO SUCH ROUTE ADDED
         if ($callback === false) :
             //404 error sukurti
-            Application::$app->response->setResponseCode(404);
+            $this->response->setResponseCode(404);
             echo 'Page doesn\'t exist';
             die();
         endif;
