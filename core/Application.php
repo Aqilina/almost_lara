b309badb5c3b5a370cb20361153c3dd55af4710f
akvilina <akvilina.lapenaite@gmail.com>
2021-02-18 08:46:15 +0200

    response and app static properties. Init in constructor.

--- core/Application.php
+++ core/Application.php
@@ -23,11 +23,15 @@ class Application
     //issaugomas router - vidine savybe. paimtas per composer - automatiskai susizino
     public Router $router;
     public Request $request;
+    public Response $response; //paimti $response:  kitam faile, reikia susikurti = new Response
+    public static Application $app; //paimti $app: Application::$app-> . klase t.b. includinta. gn paimti VISAM DARBE
 
     public function __construct($rootPath)
     {
         //static property assignment - statinis pasiekiamas su ::
         self::$ROOT_DIR = $rootPath;
+        self::$app = $this; //visur aplikacijos viduj galim paimt sia savybe
+        $this->response = new Response(); //php iesko su autoload
         $this->request = new Request(); //php iesko su autoload
         $this->router = new Router($this->request); //php iesko su autoload
     }
