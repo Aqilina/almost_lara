8c7ba691dc5271991e64a35db8ac275b4bb4b71e
akvilina <akvilina.lapenaite@gmail.com>
2021-02-18 10:53:12 +0200

    'contact' get route uses SiteController.php

--- public/index.php
+++ public/index.php
@@ -12,14 +12,13 @@ use \app\controller\SiteController;
 use app\core\Application; //router inicijuojamas Application dalyje
 
 
-
 //sukuriama nauja aplikacija, kurioje aukuriamas naujas routeris(Application.php)
 //kuriant nauja klase paduodama dirname
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 $app->router->get('/about', 'about');
-$app->router->get('/contact', 'contact');
+$app->router->get('/contact', [SiteController::class, 'contact']);
 
 //CREATE POST PATH. paduodamas klases pavadinimas - kaip kontroleris, handleContact metodas - kaip metodas
 $app->router->post('/contact', [SiteController::class, 'handleContact']); //SiteController::class  - su namespace
