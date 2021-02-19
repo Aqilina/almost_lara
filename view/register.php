1e3f6254ab516d71465c061215d5bda73869b4a5
akvilina <akvilina.lapenaite@gmail.com>
2021-02-19 11:18:55 +0200

    contact to register and changed name only

---
+++ view/register.php
@@ -0,0 +1,15 @@
+<h1>This is REGISTER</h1>
+
+<!--//KURTI POST REQUEST, NEBE GET-->
+
+<form action="" method="post">
+    <div class="form-group mb-3">
+        <label for="email" Email address</label>
+        <input name="email" type="text" type="email" class="form-control" id="email">
+    </div>
+    <div class="form-group mb-3">
+        <label for="userText" Password</label>
+        <textarea name="userText" class="form-control" id="userText" cols="30" rows=""></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
