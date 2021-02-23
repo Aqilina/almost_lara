94c102859d044bd3c54d2f498c243c32f77b12e3
akvilina <akvilina.lapenaite@gmail.com>
2021-02-23 11:17:53 +0200

    used post model in PostsController.php

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -5,11 +5,28 @@ namespace app\controller;
 
 
 use app\core\Controller;
+use app\model\PostModel;
 
 class PostsController extends Controller
 {
+    public PostModel $postModel;
+
+    public function __construct()
+    {
+        $this->postModel = new PostModel();
+    }
+
     public function index()
     {
-        return $this->render('posts/posts');
+        $posts = $this->postModel->getPosts();
+        $data = [
+            'posts' => $posts,
+        ];
+        return $this->render('posts/posts', $data);
     }
 }
+
+
+//CREATE TABLE `31-almostlara`.`posts` ( `post_id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(150) NOT NULL , `body` TEXT NOT NULL , `created_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `user_id` INT NOT NULL , PRIMARY KEY (`post_id`)) ENGINE = InnoDB;
+
+//ALTER TABLE `posts` ADD CONSTRAINT `postID_to_users` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
